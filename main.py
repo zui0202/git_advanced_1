6081267a259d6e1a7166767875bbdcb745b0e642
zui0202 <zui0202@naver.com>
2023-03-30 19:20:41 +0900

    even_list

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def even_list(int_list: List[int]) -> List[int]:
         A list of even integers.
     """
     # TODO: Implement even_list
-    pass
+    return [x for x in int_list if x % 2 == 0]
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
     """
